24353e958d5c8a518f1b8e71c7e350446e74347c
saurabhkirgat09 <saurabhkirgat1999@gmail.com>
2024-07-03 21:13:03 +0530

    first commit

---
+++ MyCode.java
@@ -0,0 +1,15 @@
+public class MyCode {
+
+    public static void main(String args[]) {
+        System.out.println("Demo app for git session");
+
+        int a =10;
+        int b = 20;
+
+        a = a+b; //
+        b = a-b; // b will get a
+        a = a-b;
+
+        System.out.println("Value of a is: " +a + "Value of b is: " +b);
+    }
+}
